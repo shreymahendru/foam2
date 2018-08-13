bb909627892e05ff932212efc0649382d4c14f55
shreymahendru <shreymahendru@gmail.com>
2018-08-13 11:40:22 -0400

    context aware validator

---
+++ src/foam/dao/ContextAwareValidatingDAO.java
@@ -0,0 +1,24 @@
+/**
+ * @license
+ * Copyright 2017 The FOAM Authors. All Rights Reserved.
+ * http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+package foam.dao;
+
+import foam.core.*;
+
+public class ContextAwareValidatingDAO extends ProxyDAO {
+  protected ContextAwareValidator validator_;
+
+  public ContextAwareValidatingDAO(X x, DAO delegate, ContextAwareValidator validator) {
+    setX(x);
+    setDelegate(delegate);
+    validator_ = validator;
+  }
+
+  public FObject put_(X x, FObject obj) throws IllegalStateException {
+    validator_.validate(x, obj);
+    return super.put_(x, obj);
+  }
+}
